2d5c0a4f02a8890cb5db3b65bd7feea10b6f7142
Mirel <valerymirel@gmail.com>
2025-05-21 11:04:50 +0500

    base init

---
+++ lib/__init__.py
@@ -0,0 +1,1 @@
+from .app import say_hello
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_hello_lib",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Твоё имя",
+    description="Простая библиотека, которая говорит Hello, world!",
+    python_requires=">=3.6",
+)
