0bfa3754dbbf1414598fefdce091a10740cf5802
Yashin Mikhail <ya.misha501@gmail.com>
2025-05-24 14:04:50 +0500

    hotfix v5

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(
     author="Твоё имя",
     description="Простая библиотека, которая говорит Hello, world!",
     python_requires=">=3.6",
+    package_data={
+        "mgp": ["*.docx"],
+    },
     include_package_data=True,
 )
